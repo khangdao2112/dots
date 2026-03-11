aebe963f2ad8fd3e16f382bcaebe6b68d2f93ee3
khangdao2112 <khangdao2112@gmail.com>
2026-03-12 01:40:39 +0700

    changes 12/03

--- userscripts/hypr-switch-gpu.sh
+++ userscripts/hypr-switch-gpu.sh
@@ -6,15 +6,18 @@ nvidia() {
 }
 
 intel() {
-    echo "#env = AQ_DRM_DEVICES,/dev/dri/nvidia-gpu:/dev/dri/intel-gpu" | tee $HOME/dots/.config/hypr/custom/gpu-use.conf > /dev/null
+    echo "env = AQ_DRM_DEVICES,/dev/dri/intel-gpu:/dev/dri/nvidia-gpu" | tee $HOME/dots/.config/hypr/custom/gpu-use.conf > /dev/null
     notify-send -a "GPU Switcher" "Switched to Intel"
 }
 
+current_gpu=$(if echo $AQ_DRM_DEVICES | grep -q "nvidia-gpu"; then echo "Nvidia"; else echo "Intel"; fi)
+
 dmenu() {
-    choice=$(echo "Nvidia|Intel" | rofi -sep '|' -dmenu -l 2 -i -window-title "Switch GPU")
+    choice=$(echo "Current gpu is $current_gpu|Nvidia|Intel" | rofi -sep '|' -dmenu -l 3 -i -window-title "Switch GPU")
     case $choice in
         Nvidia) nvidia ;;
         Intel) intel ;;
+        *) exit 0 ;;
     esac
 }
 
--- userscripts/power.sh
+++ userscripts/power.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 dmenu() {
-    choice=$(echo -e "Power Off\nReboot\nLogout\nSleep\nLock" | rofi -dmenu -i -l 5 -window-title "Power Menu")
+    choice=$(echo -e "Power Off\nReboot\nLogout\nSleep\nLock\nReboot to UEFI" | rofi -dmenu -i -l 6 -window-title "Power Menu")
     case $choice in
         "Power Off") systemctl poweroff ;;
         "Reboot") systemctl reboot ;;
         "Logout") hyprctl dispatch exit ;;
         "Sleep") systemctl suspend || loginctl suspend;;
         "Lock") loginctl lock-session ;;
+        "Reboot to UEFI") systemctl reboot --firmware-setup ;;
         *) exit 0 ;;
     esac
 }
