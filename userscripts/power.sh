c9a06abb910d0c40400434b0f99560384f72536d
khangdao2112 <khangdao2112@gmail.com>
2026-08-09 15:14:33 +0700

    power.sh edit logout to support niri and remove legion gpu switch for hyprland

--- userscripts/power.sh
+++ userscripts/power.sh
@@ -5,7 +5,12 @@ dmenu() {
     case $choice in
         "Power Off") systemctl poweroff ;;
         "Reboot") systemctl reboot ;;
-	"Logout") hyprctl dispatch 'hl.dsp.exit()' ;;
+	    "Logout") if [ $XDG_CURRENT_DESKTOP == "niri" ];
+            then niri msg action quit 
+                elif [ $XDG_CURRENT_DESKTOP == "hyprland" ];
+            then hyprctl dispatch 'hl.dsp.exit()'
+                fi
+            ;;
         "Sleep") systemctl suspend || loginctl suspend;;
         "Lock") loginctl lock-session ;;
         "Reboot to UEFI") systemctl reboot --firmware-setup ;;
